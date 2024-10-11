6ae9f467cf303fc4d5ac7ae04cb18d149a110ce9
Sidney Shogo Kai <shogo12000@hotmail.com>
2024-10-11 15:53:05 -0700

    feat: [EC-198] create JoinInBlockScreen (#19)

--- composeApp/src/commonMain/kotlin/com/europa/sightup/presentation/screens/exercise/ExerciseScreen.kt
+++ composeApp/src/commonMain/kotlin/com/europa/sightup/presentation/screens/exercise/ExerciseScreen.kt
@@ -1,6 +1,5 @@
 package com.europa.sightup.presentation.screens.exercise
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,14 +7,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -91,8 +88,6 @@ fun ExerciseItemCard(exercise: ExerciseResponse) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(top = 8.dp, bottom = 8.dp)
-            .border(width = 1.dp, color = Color.LightGray, shape = RoundedCornerShape(8.dp))
-            .padding(16.dp)
     ) {
         CardExercise(Modifier, exercise)
     }
