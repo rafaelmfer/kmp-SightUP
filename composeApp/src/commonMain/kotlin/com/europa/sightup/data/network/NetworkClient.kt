df714c8e6841c6e8990bd7baf1f74e132e230bb6
Rafael Ferreira <rafaelmfer@hotmail.com>
2024-11-18 01:40:33 -0800

    fix: logging ktor of endpoints

--- composeApp/src/commonMain/kotlin/com/europa/sightup/data/network/NetworkClient.kt
+++ composeApp/src/commonMain/kotlin/com/europa/sightup/data/network/NetworkClient.kt
@@ -6,6 +6,10 @@ import io.ktor.client.HttpClient
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.SIMPLE
 import io.ktor.client.request.header
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
@@ -31,9 +35,10 @@ object NetworkClient {
                 connectTimeoutMillis = TIMEOUT
                 socketTimeoutMillis = TIMEOUT
             }
-//            install(Logging) {
-//                level = LogLevel.BODY
-//            }
+            install(Logging) {
+                logger = Logger.SIMPLE
+                level = LogLevel.BODY
+            }
 
             defaultRequest {
                 val token = kVaultStorage.get(JWT_TOKEN)
